ff6526ff6c4f5c7ff5050e1e543ed9814c2e77a2
Johannes Martinez Calzada <spamreceiver23@gmail.com>
2018-12-10 15:41:11 -0500

    Update tag.lisp

--- tag.lisp
+++ tag.lisp
@@ -5,9 +5,9 @@
 	   #:get-all-tags))
 (in-package :tag)
 
-(defgeneric tag          (object tag) (:documentation "Adds TAG '(:TAG . VALUE) to list of tags in OBJECT."))
+(defgeneric tag          (object tag) (:documentation "Adds '(:TAG . VALUE) to list of tags in OBJECT."))
 (defgeneric get-tag      (object tag) (:documentation "Gets :TAG from OBJECT."))
-(defgeneric get-all-tags (object)     (:documentation "Returns all TAGS for given OBJECT.")) ;p or alist?
+(defgeneric get-all-tags (object)     (:documentation "Returns all TAGS for given OBJECT as alist."))
 
 ;;;examples
 

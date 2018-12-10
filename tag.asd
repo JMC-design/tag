7ce789e04b305734bb918e55ec08acecad862d6f
Johannes Martinez <spamreceiver23@gmail.com>
2018-12-10 15:22:43 -0500

    First commit.

---
+++ tag.asd
@@ -0,0 +1,7 @@
+(in-package :asdf-user)
+(defsystem "tag"
+  :description "Generic tag functions."
+  :version "0.0.1"
+  :author "Johannes Martinez Calzada"
+  :licence "MIT"
+  :components ((:file "tag")))
---
+++ tag.lisp
@@ -0,0 +1,37 @@
+(defpackage tag
+  (:use :cl)
+  (:export #:tag
+	   #:get-tag
+	   #:get-all-tags))
+(in-package :tag)
+
+(defgeneric tag          (object tag) (:documentation "Adds TAG '(:TAG . VALUE) to list of tags in OBJECT."))
+(defgeneric get-tag      (object tag) (:documentation "Gets :TAG from OBJECT."))
+(defgeneric get-all-tags (object)     (:documentation "Returns all TAGS for given OBJECT.")) ;p or alist?
+
+;;;examples
+
+;;symbol tags
+(defmethod tag ((sym symbol) (tag cons))
+  "Tags a symbol given a cons."
+  (setf (get sym (car tag)) (cdr tag) ))
+(defmethod get-tag ((sym symbol) tag)
+  "gets a tag from a symbol"
+  (get sym tag))
+(defmethod get-all-tags ((sym symbol))
+  (plist->alist (symbol-plist sym)))
+
+;;xlib:windows
+(defmethod tag ((win xlib:window) tag)
+  (setf (getf (xlib:window-plist win) (car tag)) (cdr tag)))
+(defmethod get-tag ((win xlib:window) tag)
+  (getf (xlib:window-plist win) tag))
+(defmethod get-all-tags ((win xlib:window))
+  (plist->alist (symbol-plist sym)))
+
+;; utility
+(defun plist->alist (lst)
+  (loop :for tag := (pop lst)
+     :for value := (pop lst)
+     :while (and tag value)
+     :collect (cons tag value)))

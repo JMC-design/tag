1afa89e94650c93eaae311457237b54cc39bfd8b
Johannes Martinez Calzada <johannes.martinez@gmail.com>
2019-10-21 09:21:08 -0400

    update

--- tag.lisp
+++ tag.lisp
@@ -1,39 +1,42 @@
 (defpackage tag
   (:use :cl)
-  (:export #:tag
-	   #:get-tag
-	   #:get-all-tags))
+  (:shadow GET REMOVE)
+  (:export #:add
+	   #:get
+	   #:remove
+	   #:get-all))
 (in-package :tag)
 
-(defgeneric tag          (object tag) (:documentation "Adds '(:TAG . VALUE) to list of tags in OBJECT."))
-(defgeneric get-tag      (object tag) (:documentation "Gets :TAG from OBJECT."))
-(defgeneric get-all-tags (object)     (:documentation "Returns all TAGS for given OBJECT as alist."))
+(defgeneric add          (object tag) (:documentation "Adds '(:TAG . VALUE) to list of tags in OBJECT."))
+(defgeneric get          (object tag) (:documentation "Gets :TAG from OBJECT."))
+(defgeneric remove       (object tag) (:documentation "Removes :TAG from OBJECT."))
+(defgeneric get-all      (object)     (:documentation "Returns all TAGS for given OBJECT as alist."))
 
 ;;;examples
+;; utility
+(declaim (inline plist->alist))
+(defun plist->alist (list)
+  (loop :for tag := (pop list)
+	:for value := (pop list)
+	:while (and tag value)
+	:collect (cons tag value)))
 
 ;;symbol tags
-(defmethod tag ((sym symbol) (tag cons))
-  "Tags a symbol given a cons."
-  (setf (get sym (car tag)) (cdr tag) ))
-(defmethod get-tag ((sym symbol) tag)
-  "gets a tag from a symbol"
-  (get sym tag))
-(defmethod get-all-tags ((sym symbol))
-  (plist->alist (symbol-plist sym)))
+(defmethod add     ((sym symbol) (tag cons)) (setf (get sym (car tag)) (cdr tag) ))
+(defmethod get     ((sym symbol) tag)        (get sym tag))
+(defmethod remove  ((sym symbol) tag)        (remprop sym tag))
+(defmethod get-all ((sym symbol))            (plist->alist (symbol-plist sym)))
 
+;;; The obvious
+(defmethod add     ((list list) (tag cons))  (append list (list (car tag) (cdr tag))))
+(defmethod get     ((list list) tag)         (getf list tag))
+(defmethod remove  ((list list) tag)         (remf list tag))
+(defmethod get-all ((list list))             list)
+
+;; have function to go through source and replace all instances of a generic function with an inlined function
 ;;xlib:windows
 #+clx(progn
-(defmethod tag ((win xlib:window) tag)
-  (setf (getf (xlib:window-plist win) (car tag)) (cdr tag)))
-(defmethod get-tag ((win xlib:window) tag)
-  (getf (xlib:window-plist win) tag))
-(defmethod get-all-tags ((win xlib:window))
-  (plist->alist (xlib:window-plist win)))
-)
-
-;; utility
-(defun plist->alist (lst)
-  (loop :for tag := (pop lst)
-     :for value := (pop lst)
-     :while (and tag value)
-     :collect (cons tag value)))
+(defmethod add     ((win xlib:window) tag) (setf (getf (xlib:window-plist win) (car tag)) (cdr tag)))
+(defmethod get     ((win xlib:window) tag) (getf (xlib:window-plist win) tag))
+(defmethod remove  ((win xlib:window) tag) (remf (xlib:window-plist win) tag))
+(defmethod get-all ((win xlib:window))     (plist->alist (xlib:window-plist win))))
